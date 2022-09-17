ed4ad809dfe8efc28b36fc7c3080494d6c4a0647
jglassberg <102921191+jglassberg@users.noreply.github.com>
2022-09-17 11:16:52 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,4 +6,19 @@ multiple_linear_regression <- lm(mpg ~ vehicle_weight + vehicle_length + spoiler
 
 summary(multiple_linear_regression)
 
+Suspension_Coil <- read.csv("Suspension_Coil.csv", header = TRUE, sep = ",")
+
+total_summary <- Suspension_Coil %>% 
+  summarize(sd_PSI = sd(PSI, na.rm = T),
+            mean_PSI = mean(PSI, na.rm = T),
+            median_PSI = median(PSI, na.rm = T),
+            variance_PSI = var(PSI, na.rm = T))
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(sd_PSI = sd(PSI, na.rm = T),
+            mean_PSI = mean(PSI, na.rm = T),
+            median_PSI = median(PSI, na.rm = T),
+            variance_PSI = var(PSI, na.rm = T))
+
+
 

655d131490338c9fb0fc490bc2a37e6f854f4b16
jglassberg <102921191+jglassberg@users.noreply.github.com>
2022-09-17 10:31:13 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,7 @@ library("dplyr")
 
 MechaCar_mpg <- read.csv("MechaCar_mpg.csv", header = TRUE, sep = ",")
 
-multiple_linear_regression <- lm(vehicle_weight ~ vehicle_length + spoiler_angle + ground_clearance + AWD + mpg, data = MechaCar_mpg)
+multiple_linear_regression <- lm(mpg ~ vehicle_weight + vehicle_length + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 
 summary(multiple_linear_regression)
 
